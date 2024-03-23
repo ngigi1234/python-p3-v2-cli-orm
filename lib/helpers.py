2a6d1b4301d045d4eb37bc0f88162914f33bff70
ngigi1234 <francis.ngigi1@student.moringaschool.com>
2024-03-23 11:13:07 +0300

    Done

--- lib/helpers.py
+++ lib/helpers.py
@@ -12,22 +12,21 @@ def exit_program():
 def list_departments():
     departments = Department.get_all()
     for department in departments:
-        print(department)      
+        print(department)
+
 
 def find_department_by_name():
     name = input("Enter the department's name: ")
     department = Department.find_by_name(name)
     print(department) if department else print(
-        f'Department {name} not found'
-    )
+        f'Department {name} not found')
 
 
 def find_department_by_id():
+    #use a trailing underscore not to override the built-in id function
     id_ = input("Enter the department's id: ")
     department = Department.find_by_id(id_)
-    print(department) if department else print(
-        f'Department {id_} not found'
-    )
+    print(department) if department else print(f'Department {id_} not found')
 
 
 def create_department():
@@ -42,7 +41,7 @@ def create_department():
 
 def update_department():
     id_ = input("Enter the department's id: ")
-    if department:= Department.find_by_id(id_):
+    if department := Department.find_by_id(id_):
         try:
             name = input("Enter the department's new name: ")
             department.name = name
@@ -53,14 +52,14 @@ def update_department():
             print(f'Success: {department}')
         except Exception as exc:
             print("Error updating department: ", exc)
-        else:
-            print(f'Department {id_} not found')
+    else:
+        print(f'Department {id_} not found')
 
 
 def delete_department():
     id_ = input("Enter the department's id: ")
     if department := Department.find_by_id(id_):
-        Department.delete(department)
+        department.delete()
         print(f'Department {id_} deleted')
     else:
         print(f'Department {id_} not found')
@@ -70,6 +69,7 @@ def delete_department():
 
 def list_employees():
     pass
+
 
 def find_employee_by_name():
     pass
